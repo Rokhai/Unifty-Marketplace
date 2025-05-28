aeebcb7e5e81787ea3a67407142eb8749dd70e9c
Rosgen Hizer <rokhai.dev@gmail.com>
2025-05-28 17:33:56 +0800

    feat: add RolesAndPermissionsSeeder

---
+++ database/seeders/RolesAndPermissionsSeeder.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Spatie\Permission\Models\Permission;
+use Spatie\Permission\Models\Role;
+
+class RolesAndPermissionsSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        // Create Role
+        $adminRole = Role::create(['name' => 'admin']);
+        $vendorRole = Role::create(['name' => 'vendor']);
+        $customerRole = Role::create(['name' => 'customer']);
+
+
+        // Create Permissions
+        // Admin Permissions
+        $manageUsers = Permission::create(['name' => 'manage users']);
+
+        // Vendor Permissions
+        $manageProducts = Permission::create(['name' => 'manage products']);
+        $manageOrders = Permission::create(['name' => 'manage orders']);
+        $viewReports = Permission::create(['name' => 'view reports']);
+
+
+        // Customer Permissions
+        $viewProducts = Permission::create(['name' => 'view products']);
+        $placeOrders = Permission::create(['name' => 'place orders']);
+        $viewOrderHistory = Permission::create(['name' => 'view order history']);
+
+        // Assign Permissions to Roles
+
+        // Admin Role Permissions
+        $adminRole->givePermissionTo([
+            $manageUsers,
+            $manageProducts,
+            $manageOrders,
+            $viewReports,
+        ]);
+
+        // Vendor Role Permissions
+        $vendorRole->givePermissionTo([
+            $manageProducts,
+            $manageOrders,
+            $viewReports,
+        ]);
+
+        // Customer Role Permissions
+        $customerRole->givePermissionTo([
+            $viewProducts,
+            $placeOrders,
+            $viewOrderHistory,
+        ]);
+    }
+}
