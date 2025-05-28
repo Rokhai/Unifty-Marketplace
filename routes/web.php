0befb0989d993e83beb6f4dff372c28ec2f42ea2
Rosgen Hizer <rokhai.dev@gmail.com>
2025-05-28 16:10:23 +0800

    feat: add store route & include in web

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,8 @@
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 
+use App\Http\Controllers\MyStoreController;
+
 Route::get('/', function () {
     return Inertia::render('welcome');
 })->name('home');
@@ -11,7 +13,10 @@ Route::middleware(['auth', 'verified'])->group(function () {
     Route::get('dashboard', function () {
         return Inertia::render('dashboard');
     })->name('dashboard');
+    Route::get('market', [\App\Http\Controllers\MarketController::class, 'create'])
+        ->name('market');
 });
 
 require __DIR__.'/settings.php';
 require __DIR__.'/auth.php';
+require __DIR__.'/store.php';
